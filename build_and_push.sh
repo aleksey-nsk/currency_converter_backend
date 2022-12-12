5f728c920aa8eb25aba781651719895c2fbfef3a
Aleksey Zhdanov <alex.zhd@email.test>
2022-12-12 21:34:14 +0700

    update 2

--- build_and_push.sh
+++ build_and_push.sh
@@ -2,10 +2,10 @@ echo "********* CREATE JAR *********"
 mvn clean package
 
 echo "********** COPY JAR **********"
-cp --force target/demo-0.0.1-SNAPSHOT.jar docker
+cp --force target/demo-0.0.1-SNAPSHOT.jar docker/build
 
 echo "******** BUILD IMAGE *********"
-docker build -t alexz2/converter_back:1.0.0 docker -f docker/Dockerfile
+docker build -t alexz2/converter_back:1.0.0 docker/build -f docker/build/Dockerfile
 
 echo "***** PUSH TO DOCKER HUB *****"
 docker push alexz2/converter_back:1.0.0
